5bdef24890a27439c6b06d93b6fb993b022b5cca
Giulia De Santis <ingegneriagiulia@gmail.com>
2025-03-20 17:48:36 +0100

    Update main.cpp
    corrections

--- Exercise2/main.cpp
+++ Exercise2/main.cpp
@@ -6,18 +6,22 @@
 
 
 double map_values(double x){
-	return -7.0/4.0 +3.0/4.0 * x; // la formula da [a,b] a [c,d] sarebbe: x' = [(c-d)*x+a*d-b*c]/(a-b)
+	int a = 1;
+	int b = 5;
+	int c = -1;
+	int d = 2;
+	return [(c-d)*x+a*d-b*c]/(a-b); // la formula da [a,b] a [c,d] sarebbe: x' = [(c-d)*x+a*d-b*c]/(a-b)
 }
 
 int main()
 {
 	std::ifstream file("data.txt");
-	if (file.fail()) // per controllare se tutto è andato correttamente o meno...
+	if (file.fail()) // per controllare se il file si apre correttamente
 	{
 		std::cerr << "errore nell'apertura del file" << std::endl;
 		return 1;
 	}
-	double val;//creo variabile ausiliaria dove vado a memorizzare i valori letti dal file
+	double val; //creo variabile ausiliaria dove vado a memorizzare i valori letti dal file
 	double mean = 0.0;
 	unsigned int k = 0;
 	double x = 0.0;
@@ -28,7 +32,6 @@ int main()
 		std :: cout << std::scientific << std::setprecision(16) << mean << std::endl;
 		k++;
 	}
-	
 	file.close();
     return 0;
 }
